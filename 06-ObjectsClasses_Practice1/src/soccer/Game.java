fc83de13dcf6640c264d7414a6d0f8ac220e6e43
Edgar Anaya <eanaya@ipn.mx>
2023-01-19 13:20:21 -0600

    Modificacion

--- 06-ObjectsClasses_Practice1/src/soccer/Game.java
+++ 06-ObjectsClasses_Practice1/src/soccer/Game.java
@@ -1,4 +1,9 @@
 /*
+ * Autor: Edgar Anaya
+ * Fecha: 19.01.23
+ */
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
